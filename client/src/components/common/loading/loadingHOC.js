b2c4659e625e576f496b77b859339b415d2119e7
Viktoria <vika.bondareva.2014@mail.ru>
2019-03-31 13:15:52 +0300

    hoc leader

---
+++ client/src/components/common/loading/loadingHOC.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import './loadingHOC.css';
+
+const isEmpty = (prop) => (
+    prop === null ||
+    prop === undefined ||
+    (prop.hasOwnProperty('length') && prop.length === 0) ||
+    (prop.constructor === Object && Object.keys(prop).length === 0)
+  );
+  
+
+const loadingHOC = (loadingProp) => (WrappedComponent)=>{
+    return class LoadingHOC extends Component{
+        render(){
+            return this.props[loadingProp]===true
+                ?<div className="loader"/>
+                : <WrappedComponent  {...this.props}/>
+        }
+    }
+}
+
+export default loadingHOC;
