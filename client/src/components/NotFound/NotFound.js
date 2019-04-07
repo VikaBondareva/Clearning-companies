2fbcbfe678da18fce62dd43accf65a0b07264b40
Viktoria <vika.bondareva.2014@mail.ru>
2019-04-07 11:45:42 +0300

    404 error page

--- client/src/components/NotFound/NotFound.js
+++ client/src/components/NotFound/NotFound.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './style.css';
 
 export default function NotFound (props){
 
     return ( 
-        <div>
-            <p>Not found {props.match.url}</p>
+        <div className="not-found">
+                 <h1 >404</h1>
+                <p>This resource cannot be found {props.match.url}.</p>
         </div>
     );
 }
